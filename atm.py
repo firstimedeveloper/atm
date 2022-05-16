766fcad9a7dc24252cee0c332f773b246e64d03f
Jun Hyuk Han <hanjun.1998@gmail.com>
2022-05-16 20:28:56 +0900

    added a small test to simulate a working atm

--- atm.py
+++ atm.py
@@ -22,6 +22,7 @@ class Bank:
 		account = Account(self.id_counter, pin, initial_cash)
 		self.accounts.add(account)
 		self.id_counter += 1
+		return (account)
 	def check_pin(self, card_num, pin):
 		for account in self.accounts:
 			if card_num in account:
@@ -31,9 +32,9 @@ class Bank:
 	def get_balance(self, card_num, pin):
 		err, account = self.check_pin(card_num, pin)
 		if err == False:
-			return False
+			return False, 0
 		else:
-			return account.cash
+			return True, account.cash
 	def deposit(self, card_num, pin, cash):
 		err, account = self.check_pin(card_num, pin)
 		if err == False:
@@ -53,23 +54,59 @@ class Bank:
 
 
 class Atm:
+	_authorized = False
+	_card_num = 0
+	_pin = 0
 	def __init__(self, bank):
 		self.bank = bank
-		self.account = None
-		self.authorized = False
 	def insert_card(self, card_num, pin):
 		if self.bank.check_pin(card_num, pin):
 			print("Valid Card")
-			self.authorized = True
+			self._authorized = True
+			self._card_num = card_num
+			self._pin = pin
+			return True
 		else:
 			print("Invalid Card")
-			self.authorized = False
+			self._authorized = False
+			return False
 	def see_balance(self):
-		if self.authorized == False:
+		if self._authorized == False:
 			print("Not Authorized")
+			return False
 		else:
-			print()
+			err, balance = self.bank.get_balance(self._card_num, self._pin)
+			if err == False:
+				print("Something went wrong...")
+				return False
+			else:
+				print("Balance: $" + str(balance))
+			return True
+	def end(self):
+		self._card_num = 0
+		self._pin = 0
+		self._authorized = False
+	# testing ATM functionality
+	def run(self):
+		n = int(input("Enter the number of accounts to be created: "))
+		for i in range(1, n+1):
+			pin, cash = map(int, input("Account " + str(i) + ": Enter the pin number, and starting cash amount (pin, cash): ").split())
+			account = self.bank.add_account(pin, cash)
+			print(account)
+		while (True):
+			print("Starting the ATM simulation.")
+			card_num = int(input("Please insert your card: "))
+			pin = int(input("Please insert the pin number: "))
+			err = self.insert_card(card_num, pin)
+			break
 
+
+
+
+
+
+
+"""
 bank = Bank()
 bank.add_account(1234, 0)
 bank.add_account(12345, 0)
@@ -83,4 +120,7 @@ for account in bank.accounts:
 
 account = Account(0, 1234)
 atm = Atm(bank)
-atm.insert_card(account.card_num, account.pin)
+atm.insert_card(account.card_num, account.pin) """
+bank = Bank()
+atm = Atm(bank)
+atm.run()
